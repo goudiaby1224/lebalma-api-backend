0be3a6f8ad0736d5843b1445dee54f145b8faa4f
Noel Goudiaby <goudiaby1224@gmail.com>
2020-12-23 12:45:06 -0500

    Specify type

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO SERVICEOUMOU(id, nom, responsable)
 values (1, 'Lamine Doucoure Gorgui Ngor keng', 'Responsable service');
-INSERT INTO PARTENAIRE(id, raisonsocial, adresse, telephone, responsable, mail, dateMiseEnService)
-values (1, 'Operateur telephonique', 'adresse', '13243243242', 'responsable', 'mail@mail.com', current_date);
+INSERT INTO PARTENAIRE(id, raisonsocial, adresse, telephone, responsable, mail, dateMiseEnService,type)
+values (1, 'Operateur telephonique', 'adresse', '13243243242', 'responsable', 'mail@mail.com', current_date,'EMPLOYEUR');
