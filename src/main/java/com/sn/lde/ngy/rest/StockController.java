0d975ca24bca516034b96f8b9ff107a8f9992e0f
Noel Goudiaby <goudiaby1224@gmail.com>
2021-01-02 17:23:34 -0500

    Init stock management

---
+++ src/main/java/com/sn/lde/ngy/rest/StockController.java
@@ -0,0 +1,10 @@
+package com.sn.lde.ngy.rest;
+
+import com.sn.lde.ngy.utils.ConstantsPath;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ConstantsPath.MAIN_LEBALMA_PATH+"users")
+public class StockController {
+}
---
+++ src/main/java/com/sn/lde/ngy/utils/ConstantsPath.java
@@ -0,0 +1,5 @@
+package com.sn.lde.ngy.utils;
+
+public class ConstantsPath {
+    public static  final String MAIN_LEBALMA_PATH = "/lebalma/";
+}
